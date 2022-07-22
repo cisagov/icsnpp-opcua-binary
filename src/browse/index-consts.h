8de3868cc1c482c41d5cee005f20f858e3e5aca8
Melanie Pierce <melanie.pierce@inl.gov>
2022-07-22 16:29:35 -0600

    Add browseNext service to browse

--- src/browse/index-consts.h
+++ src/browse/index-consts.h
@@ -15,41 +15,45 @@
 // based on the parsed values from Browse_Req and Browse_Res
 //
 #define BROWSE_OPCUA_ID_LINK_IDX                                3
+#define BROWSE_SERVICE_TYPE_IDX                                 4
 // The ID_ENCODING_MASK is also used as the offset
-#define BROWSE_VIEW_ID_ENCODING_MASK_IDX                        4
-#define BROWSE_VIEW_ID_NAMESPACE_IDX                            5
-#define BROWSE_VIEW_ID_NUMERIC_IDX                              6
-#define BROWSE_VIEW_ID_STRING_IDX                               7
-#define BROWSE_VIEW_ID_GUID_IDX                                 8
-#define BROWSE_VIEW_ID_OPAQUE_IDX                               9
+#define BROWSE_VIEW_ID_ENCODING_MASK_IDX                        5
+#define BROWSE_VIEW_ID_NAMESPACE_IDX                            6
+#define BROWSE_VIEW_ID_NUMERIC_IDX                              7
+#define BROWSE_VIEW_ID_STRING_IDX                               8
+#define BROWSE_VIEW_ID_GUID_IDX                                 9
+#define BROWSE_VIEW_ID_OPAQUE_IDX                              10
 
-#define BROWSE_VIEW_DESCRIPTION_TIMESTAMP_IDX                  10 
-#define BROWSE_VIEW_DESCRIPTION_VIEW_VERSION_IDX               11 
-#define BROWSE_REQ_MAX_REFS_IDX                                12 
+#define BROWSE_VIEW_DESCRIPTION_TIMESTAMP_IDX                  11 
+#define BROWSE_VIEW_DESCRIPTION_VIEW_VERSION_IDX               12 
+#define BROWSE_REQ_MAX_REFS_IDX                                13 
 
-#define BROWSE_DESCRIPTION_ID_ENCODING_MASK_IDX                13
-#define BROWSE_DESCRIPTION_ID_NAMESPACE_IDX                    14
-#define BROWSE_DESCRIPTION_ID_NUMERIC_IDX                      15
-#define BROWSE_DESCRIPTION_ID_STRING_IDX                       16
-#define BROWSE_DESCRIPTION_ID_GUID_IDX                         17
-#define BROWSE_DESCRIPTION_ID_OPAQUE_IDX                       18
+#define BROWSE_DESCRIPTION_ID_ENCODING_MASK_IDX                14
+#define BROWSE_DESCRIPTION_ID_NAMESPACE_IDX                    15
+#define BROWSE_DESCRIPTION_ID_NUMERIC_IDX                      16
+#define BROWSE_DESCRIPTION_ID_STRING_IDX                       17
+#define BROWSE_DESCRIPTION_ID_GUID_IDX                         18
+#define BROWSE_DESCRIPTION_ID_OPAQUE_IDX                       19
 
-#define BROWSE_DIRECTION_ID_IDX                                19
+#define BROWSE_DIRECTION_ID_IDX                                20
 
-#define BROWSE_DESCRIPTION_REF_ID_ENCODING_MASK_IDX            20
-#define BROWSE_DESCRIPTION_REF_ID_NAMESPACE_IDX                21
-#define BROWSE_DESCRIPTION_REF_ID_NUMERIC_IDX                  22
-#define BROWSE_DESCRIPTION_REF_ID_STRING_IDX                   23
-#define BROWSE_DESCRIPTION_REF_ID_GUID_IDX                     24
-#define BROWSE_DESCRIPTION_REF_ID_OPAQUE_IDX                   25
+#define BROWSE_DESCRIPTION_REF_ID_ENCODING_MASK_IDX            21
+#define BROWSE_DESCRIPTION_REF_ID_NAMESPACE_IDX                22
+#define BROWSE_DESCRIPTION_REF_ID_NUMERIC_IDX                  23
+#define BROWSE_DESCRIPTION_REF_ID_STRING_IDX                   24
+#define BROWSE_DESCRIPTION_REF_ID_GUID_IDX                     25
+#define BROWSE_DESCRIPTION_REF_ID_OPAQUE_IDX                   26
 
-#define BROWSE_DESCRIPTION_INCLUDE_SUBTYPES_IDX                26 
-#define BROWSE_DESCRIPTION_NODE_CLASS_MASK_IDX                 27
-#define BROWSE_DESCRIPTION_RESULT_MASK_IDX                     28
+#define BROWSE_DESCRIPTION_INCLUDE_SUBTYPES_IDX                27 
+#define BROWSE_DESCRIPTION_NODE_CLASS_MASK_IDX                 28
+#define BROWSE_DESCRIPTION_RESULT_MASK_IDX                     29
 
-#define BROWSE_RESPONSE_STATUS_CODE_IDX                        29
-#define BROWSE_RESPONSE_CONTINUATION_POINT_IDX                 30
-#define BROWSE_RESPONSE_NUM_REFERENCES_IDX                     31
+#define BROWSE_NEXT_RELEASE_CONTINUATION_POINTS_IDX            30
+#define BROWSE_NEXT_CONTINUATION_POINT_IDX                     31
+
+#define BROWSE_RESPONSE_STATUS_CODE_IDX                        32
+#define BROWSE_RESPONSE_CONTINUATION_POINT_IDX                 33
+#define BROWSE_RESPONSE_NUM_REFERENCES_IDX                     34
 
 
 // Log reference events in a separate file
